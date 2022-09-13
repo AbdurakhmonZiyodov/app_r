44e72cbea552ad05af63212ef23a7bea167411ce
Abdumutal Abdusamatov <abdusamatov@mercurydevelopment.com>
2022-09-13 11:52:47 +0500

    react-native permissions working

--- App.tsx
+++ App.tsx
@@ -1,120 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { Button, StyleSheet, Text, View, PermissionsAndroid } from 'react-native'
+import React from 'react'
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker'
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const galeriyaHandler = async () => {
+
+    try {
+      const grantedstorage = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+      if (grantedstorage === PermissionsAndroid.RESULTS.GRANTED) {
+        const res = await launchImageLibrary({
+          mediaType: "photo"
+        })
+      }
+    } catch (error) {
+      console.log(error)
+
+    }
+  }
+  const cameraHandler = async () => {
+
+    try {
+      const grantedcamera = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+      if (grantedcamera === PermissionsAndroid.RESULTS.GRANTED) {
+        const res = await launchCamera({
+          mediaType: "photo",
+          cameraType: "back"
+        })
+        console.log(res)
+      }
+    } catch (error) {
+      console.log(error)
+
+    }
+  }
+
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundColor: "white"
+      }}
+    >
+      <Text
+        style={{
+          color: "#000",
+          fontSize: 20
+        }}
+      >App</Text>
+
+      <Button
+        title="galeriyaga ut"
+        onPress={galeriyaHandler}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+      <Button
+        title="cameraga ut"
+        onPress={cameraHandler}
+      />
+    </View>
+  )
+}
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({})
